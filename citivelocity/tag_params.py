e5aa56aa3942997021df5b7b11546ba734170361
jblloyd14 <lloyd.brad@gmail.com>
2025-06-13 22:10:38 +0100

    added vol related tag params

---
+++ citivelocity/tag_params.py
@@ -0,0 +1,22 @@
+eq_vol_product_types = ["ETF", "Indices", "Stocks", "Index Correlation"]
+eq_vol_types = ["Spot Moneyness", "Forward Moneyness", "Delta", "Realized"]
+moneyness_strikes = [
+    20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 82.5, 85, 87.5, 90, 92.5, 95, 97.5, 100,
+    102.5, 105, 107.5, 110, 112.5, 115, 117.5, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165,
+    170, 175, 180, 185, 190, 195, 200
+
+]
+delta_strikes = ['M1', 'M5', 'M10', 'M25', 'M50', 'M75', 'C75', 'C50', 'C25', 'C10', 'C5', 'C1']
+
+
+eq_vol_tenors = [
+    '1W', '2W', '1M', '2M', '3M', '4M', '5M', '6M', '7M', '8M', '9M', '1Y', '2Y', '3Y'
+]
+
+rates_vol_ccy = ['USD', 'EUR', 'GBP', 'JPY', 'CHF', 'AUD', 'KRW', 'NZD', 'NOK', 'SEK', 'DKK']
+
+rates_vol_cat = ['ATM_RFR']
+
+rates_vol_expiry = ['1M', '2M', '3M', '6M', '9M', '1Y', '18M' '2Y', '3Y', '4Y', '5Y',
+                      '7Y', '10Y', '12Y', '15Y', '20Y', '30Y']
+rates_vol_tenors = ['3M', '1Y', '2Y', '3Y', '5Y', '7Y', '10Y', '15Y', '20Y', '30Y']
